202b3add0cc14b8bdfff6255bd84e711d57908b9
pintarkantor-prog <pintarkantor@gmail.com>
2026-02-05 04:39:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 st.title("📸 PINTAR MEDIA")
-st.info("Mode: v9.15 | DROPBOX MASTER SYNC | ULTIMATE MENDUNG READY ❤️")
+st.info("MASIH UJI COBA DULU GUYS ❤️")
 
 # ==============================================================================
 # 3. LOGIKA MASTER SYNC (SESSION STATE ENGINE)
@@ -275,3 +275,4 @@ if st.button("🚀 GENERATE ALL PROMPTS", type="primary"):
 
 st.sidebar.markdown("---")
 st.sidebar.caption("PINTAR MEDIA Storyboard v9.15 - Dropbox Sync Edition")
+
