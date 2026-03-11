21f9c3d263b22d069bdaffdbf0b379d07da51fae
pintarkantor-prog <pintarkantor@gmail.com>
2026-03-12 01:48:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5291,14 +5291,14 @@ def tampilkan_database_channel():
             df_display = df_j.sort_values(['HP_N', 'PAGI'])
             
             # PISAHKAN LIST HP JADI 2 KELOMPOK
-            list_hp_tim1 = [h for h in df_display['HP'].unique() if pd.to_numeric(h, errors='coerce') <= 14]
-            list_hp_tim2 = [h for h in df_display['HP'].unique() if pd.to_numeric(h, errors='coerce') > 14]
+            list_hp_tim1 = [h for h in df_display['HP'].unique() if pd.to_numeric(h, errors='coerce') <= 16]
+            list_hp_tim2 = [h for h in df_display['HP'].unique() if pd.to_numeric(h, errors='coerce') > 16]
             
             # Gabungkan jadi satu list besar tapi kita kasih pembatas (marker)
             # Ini biar kodenya mirip struktur awal lo yang pake loop
             kelompok_tim = [
-                {"nama": "TIM 1 (HP 1-14)", "list": list_hp_tim1},
-                {"nama": "TIM 2 (HP 15+)", "list": list_hp_tim2}
+                {"nama": "LISA (HP 1-16)", "list": list_hp_tim1},
+                {"nama": "INGGI (HP 17-23)", "list": list_hp_tim2}
             ]
 
             html_all_pages = "" 
@@ -6090,6 +6090,7 @@ def utama():
 # --- EKSEKUSI SISTEM ---
 if __name__ == "__main__":
     utama()
+
 
 
 
