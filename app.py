440be6a9aee6c782cb762827d084536df55d0e93
pintarkantor-prog <pintarkantor@gmail.com>
2026-03-24 16:23:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5117,7 +5117,7 @@ def tampilkan_database_channel():
                 if not list_hp_unik: continue
                 
                 # Loop per 11 HP (sama kayak kode awal lo)
-                for start_idx in range(0, len(list_hp_unik), 7):
+                for start_idx in range(0, len(list_hp_unik), 8):
                     hp_halaman_ini = list_hp_unik[start_idx : start_idx + 7]
                     df_page = df_display[df_display['HP'].isin(hp_halaman_ini)]
                     
